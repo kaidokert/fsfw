effac0e9b7c46913283c45f483b37df3335e8042
Robin.Mueller <robin.mueller.m@gmail.com>
2020-05-11 12:22:06 +0200

    global printer function

---
+++ globalfunctions/printer.h
@@ -0,0 +1,12 @@
+#ifndef FRAMEWORK_GLOBALFUNCTIONS_PRINTER_H_
+#define FRAMEWORK_GLOBALFUNCTIONS_PRINTER_H_
+#include <cstdint>
+#include <cstddef>
+
+namespace printer {
+void print(uint8_t *data, size_t size);
+}
+
+
+
+#endif /* FRAMEWORK_GLOBALFUNCTIONS_PRINTER_H_ */
