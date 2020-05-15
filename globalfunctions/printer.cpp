e92c324c7ef26eef4877041df94fa2d8fa16f8db
Robin.Mueller <robin.mueller.m@gmail.com>
2020-05-15 18:47:46 +0200

    updated printer

--- globalfunctions/printer.cpp
+++ globalfunctions/printer.cpp
@@ -1,13 +1,34 @@
 #include <framework/globalfunctions/printer.h>
 #include <framework/serviceinterface/ServiceInterfaceStream.h>
 
-void printer::print(uint8_t *data, size_t size) {
+void printer::print(uint8_t *data, size_t size, OutputType type) {
 	sif::info << "StorageAccessor: Printing data: [";
+	if(type == OutputType::HEX) {
+		printer::printHex(data, size);
+	}
+	else {
+		printer::printDec(data, size);
+	}
+}
+
+void printer::printHex(uint8_t *data, size_t size) {
+	sif::info << std::hex;
 	for(size_t i = 0; i < size; i++) {
-		sif::info << std::hex << (int)data[i];
+		sif::info << "0x" << static_cast<int>(data[i]);
 		if(i < size - 1){
 			sif::info << " , ";
 		}
 	}
+	sif::info << std::dec;
 	sif::info << " ] " << std::endl;
 }
+
+void printer::printDec(uint8_t *data, size_t size) {
+	for(size_t i = 0; i < size; i++) {
+		sif::info << "0x" << static_cast<int>(data[i]);
+		if(i < size - 1){
+			sif::info << " , ";
+		}
+	}
+}
+
