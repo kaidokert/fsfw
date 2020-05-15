ebcc4742a92c6aa2152aa7cc8953a587f05fec5b
Robin.Mueller <robin.mueller.m@gmail.com>
2020-05-15 21:14:21 +0200

    removed conflict markers

--- osal/FreeRTOS/FixedTimeslotTask.cpp
+++ osal/FreeRTOS/FixedTimeslotTask.cpp
@@ -57,10 +57,6 @@ ReturnValue_t FixedTimeslotTask::startTask() {
 
 ReturnValue_t FixedTimeslotTask::addSlot(object_id_t componentId,
 		uint32_t slotTimeMs, int8_t executionStep) {
-<<<<<<< HEAD
-=======
-
->>>>>>> mueller_framework
 	if (objectManager->get<ExecutableObjectIF>(componentId) != nullptr) {
 		if(slotTimeMs == 0) {
 			// FreeRTOS throws a sanity error for zero values, so we set
