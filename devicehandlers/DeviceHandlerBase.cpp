e9a4056deb17756f70973d6f383182fbabdf90b2
Robin.Mueller <robin.mueller.m@gmail.com>
2020-05-17 15:19:24 +0200

    added DHB cookie init
    (why was that never a problem beofre????) ¯\_(ツ)_/¯

--- devicehandlers/DeviceHandlerBase.cpp
+++ devicehandlers/DeviceHandlerBase.cpp
@@ -23,8 +23,9 @@ DeviceHandlerBase::DeviceHandlerBase(object_id_t setObjectId,
 		size_t cmdQueueSize) :
 		SystemObject(setObjectId), mode(MODE_OFF), submode(SUBMODE_NONE),
 		wiretappingMode(OFF), storedRawData(StorageManagerIF::INVALID_ADDRESS),
-		deviceCommunicationId(deviceCommunication), deviceThermalStatePoolId(
-		thermalStatePoolId),deviceThermalRequestPoolId(thermalRequestPoolId),
+		deviceCommunicationId(deviceCommunication), comCookie(comCookie),
+		deviceThermalStatePoolId(thermalStatePoolId),
+		deviceThermalRequestPoolId(thermalRequestPoolId),
 		healthHelper(this,setObjectId), modeHelper(this), parameterHelper(this),
 		childTransitionFailure(RETURN_OK), fdirInstance(fdirInstance),
 		hkSwitcher(this), defaultFDIRUsed(fdirInstance == nullptr),
