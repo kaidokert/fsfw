896e7f15dc9e4e4ababcf1283e5c6be460780d8a
Robin.Mueller <robin.mueller.m@gmail.com>
2020-05-29 14:16:44 +0200

    addd new timeout value

--- osal/FreeRTOS/Mutex.cpp
+++ osal/FreeRTOS/Mutex.cpp
@@ -3,27 +3,31 @@
 #include <framework/serviceinterface/ServiceInterfaceStream.h>
 
 const uint32_t MutexIF::NO_TIMEOUT = 0;
+const uint32_t MutexIF::MAX_TIMEOUT = portMAX_DELAY;
 
 Mutex::Mutex() {
 	handle = xSemaphoreCreateMutex();
-	if(handle == NULL) {
-		sif::error << "Mutex creation failure" << std::endl;
+	if(handle == nullptr) {
+		sif::error << "Mutex: Creation failure" << std::endl;
 	}
 }
 
 Mutex::~Mutex() {
-	if (handle != 0) {
+	if (handle != nullptr) {
 		vSemaphoreDelete(handle);
 	}
 
 }
 
 ReturnValue_t Mutex::lockMutex(uint32_t timeoutMs) {
-	if (handle == 0) {
+	if (handle == nullptr) {
 		return MutexIF::MUTEX_NOT_FOUND;
 	}
-	TickType_t timeout = portMAX_DELAY;
-	if (timeoutMs != NO_TIMEOUT) {
+	TickType_t timeout = MutexIF::NO_TIMEOUT;
+	if(timeoutMs == MutexIF::MAX_TIMEOUT) {
+		timeout = MutexIF::MAX_TIMEOUT;
+	}
+	else if(timeoutMs > MutexIF::NO_TIMEOUT){
 		timeout = pdMS_TO_TICKS(timeoutMs);
 	}
 
@@ -36,7 +40,7 @@ ReturnValue_t Mutex::lockMutex(uint32_t timeoutMs) {
 }
 
 ReturnValue_t Mutex::unlockMutex() {
-	if (handle == 0) {
+	if (handle == nullptr) {
 		return MutexIF::MUTEX_NOT_FOUND;
 	}
 	BaseType_t returncode = xSemaphoreGive(handle);
