6ecf1cf534b1eaf45be731263a241b629fa3d937
Robin.Mueller <robin.mueller.m@gmail.com>
2020-06-14 19:03:28 +0200

    hk message continued

--- housekeeping/HousekeepingMessage.cpp
+++ housekeeping/HousekeepingMessage.cpp
@@ -3,9 +3,25 @@
 
 HousekeepingMessage::HousekeepingMessage(MessageQueueMessageIF* message):
 		CommandMessageBase(message) {
+	if(message->getMaximumMessageSize() < HK_MESSAGE_SIZE) {
+		sif::error << "CommandMessage::ComandMessage: Passed message buffer"
+				" can not hold minimum "<< HK_MESSAGE_SIZE
+				<< " bytes!" << std::endl;
+		return;
+	}
+	message->setMessageSize(HK_MESSAGE_SIZE);
 }
 
-HousekeepingMessage::~HousekeepingMessage() {
+HousekeepingMessage::~HousekeepingMessage() {}
+
+void HousekeepingMessage::setParameter(uint32_t parameter) {
+	std::memcpy(getData(), &parameter, sizeof(parameter));
+}
+
+uint32_t HousekeepingMessage::getParameter() const {
+	uint32_t parameter;
+	std::memcpy(&parameter, getData(), sizeof(parameter));
+	return parameter;
 }
 
 void HousekeepingMessage::setHkReportMessage(sid_t sid,
@@ -15,16 +31,15 @@ void HousekeepingMessage::setHkReportMessage(sid_t sid,
 	setParameter(storeId.raw);
 }
 
+void HousekeepingMessage::setHkDiagnosticsMessage(sid_t sid,
+		store_address_t storeId) {
+	CommandMessageBase::setCommand(DIAGNOSTICS_REPORT);
+	setSid(sid);
+	setParameter(storeId.raw);
+}
+
 size_t HousekeepingMessage::getMinimumMessageSize() const {
 	return HK_MESSAGE_SIZE;
-}
-
-size_t HousekeepingMessage::getMaximumMessageSize() const {
-	return MessageQueueMessage::MAX_MESSAGE_SIZE;
-}
-
-void HousekeepingMessage::clear() {
-	// clear IPC store where it is needed.
 }
 
 sid_t HousekeepingMessage::getSid() const {
@@ -33,14 +48,28 @@ sid_t HousekeepingMessage::getSid() const {
 	return sid;
 }
 
-uint8_t* HousekeepingMessage::getData() {
-	return internalMessage->getBuffer() + sizeof(sid_t);
-}
 
-void HousekeepingMessage::setParameter(uint32_t parameter) {
-	memcpy(getData(), &parameter, sizeof(parameter));
+sid_t HousekeepingMessage::getHkReportMessage(
+		store_address_t *storeIdToSet) const {
+	if(storeIdToSet != nullptr) {
+		*storeIdToSet = getParameter();
+	}
+	return getSid();
 }
 
 void HousekeepingMessage::setSid(sid_t sid) {
 	std::memcpy(CommandMessageBase::getData(), &sid.raw, sizeof(sid.raw));
+}
+
+
+uint8_t* HousekeepingMessage::getData() {
+	return CommandMessageBase::getData() + sizeof(sid_t);
+}
+
+const uint8_t* HousekeepingMessage::getData() const {
+	return CommandMessageBase::getData() + sizeof(sid_t);
+}
+
+void HousekeepingMessage::clear() {
+	// clear IPC store where it is needed.
 }
