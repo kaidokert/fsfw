fd8a3e9fcc4efa40f84c6ecaa9163aac9843730f
Robin.Mueller <robin.mueller.m@gmail.com>
2020-06-23 13:00:38 +0200

    new returnvalues, return failed is 0xFFFF now

--- returnvalues/HasReturnvaluesIF.h
+++ returnvalues/HasReturnvaluesIF.h
@@ -1,27 +1,19 @@
-#ifndef HASRETURNVALUESIF_H_
-#define HASRETURNVALUESIF_H_
+#ifndef FRAMEWORK_RETURNVALUES_HASRETURNVALUESIF_H_
+#define FRAMEWORK_RETURNVALUES_HASRETURNVALUESIF_H_
 
-#include <stdint.h>
 #include <framework/returnvalues/FwClassIds.h>
 #include <config/returnvalues/classIds.h>
+#include <cstdint>
 
 #define MAKE_RETURN_CODE( number )	((INTERFACE_ID << 8) + (number))
 typedef uint16_t ReturnValue_t;
 
 
-
-
-
-
 class HasReturnvaluesIF {
 public:
 	static const ReturnValue_t RETURN_OK = 0;
-	static const ReturnValue_t RETURN_FAILED = 1;
-	virtual ~HasReturnvaluesIF() {
-	}
-
+	static const ReturnValue_t RETURN_FAILED = 0xFFFF;
+	virtual ~HasReturnvaluesIF() {}
 };
-
-
 
 #endif /* HASRETURNVALUESIF_H_ */
