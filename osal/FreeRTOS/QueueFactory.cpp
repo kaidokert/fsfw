74b9aef36b2dd920565ed119172cade85bcfd421
Robin.Mueller <robin.mueller.m@gmail.com>
2020-09-22 15:33:28 +0200

    freeRTOS update

--- osal/FreeRTOS/QueueFactory.cpp
+++ osal/FreeRTOS/QueueFactory.cpp
@@ -4,18 +4,18 @@
 #include "MessageQueue.h"
 
 
-QueueFactory* QueueFactory::factoryInstance = NULL;
+QueueFactory* QueueFactory::factoryInstance = nullptr;
 
 
 ReturnValue_t MessageQueueSenderIF::sendMessage(MessageQueueId_t sendTo,
 			MessageQueueMessage* message, MessageQueueId_t sentFrom,
 			bool ignoreFault) {
-	return MessageQueue::sendMessageFromMessageQueue(sendTo, message,
-			sentFrom, ignoreFault);
+	return MessageQueue::sendMessageFromMessageQueue(sendTo,message,
+			sentFrom,ignoreFault);
 }
 
 QueueFactory* QueueFactory::instance() {
-	if (factoryInstance == NULL) {
+	if (factoryInstance == nullptr) {
 		factoryInstance = new QueueFactory;
 	}
 	return factoryInstance;
@@ -27,9 +27,9 @@ QueueFactory::QueueFactory() {
 QueueFactory::~QueueFactory() {
 }
 
-MessageQueueIF* QueueFactory::createMessageQueue(uint32_t message_depth,
+MessageQueueIF* QueueFactory::createMessageQueue(uint32_t messageDepth,
 		size_t maxMessageSize) {
-	return new MessageQueue(message_depth, maxMessageSize);
+	return new MessageQueue(messageDepth, maxMessageSize);
 }
 
 void QueueFactory::deleteMessageQueue(MessageQueueIF* queue) {
