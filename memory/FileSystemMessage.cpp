a7fe23c3004d8c933b36d3c5a23f0b8322eaa022
Robin.Mueller <robin.mueller.m@gmail.com>
2020-10-05 00:12:52 +0200

    small stuff

--- memory/FileSystemMessage.cpp
+++ memory/FileSystemMessage.cpp
@@ -1,47 +1,46 @@
 #include "FileSystemMessage.h"
 #include "../objectmanager/ObjectManagerIF.h"
 
+void FileSystemMessage::setCreateFileCommand(CommandMessage* message,
+        store_address_t storeId) {
+    message->setCommand(CREATE_FILE);
+    message->setParameter2(storeId.raw);
+}
 
 void FileSystemMessage::setDeleteFileCommand(
-		CommandMessage* message, store_address_t storageID) {
-	message->setCommand(DELETE_FILE);
-	message->setParameter2(storageID.raw);
+        CommandMessage* message, store_address_t storeId) {
+    message->setCommand(DELETE_FILE);
+    message->setParameter2(storeId.raw);
 }
 
 void FileSystemMessage::setCreateDirectoryCommand(
-		CommandMessage* message, store_address_t storageID) {
+		CommandMessage* message, store_address_t storeId) {
 	message->setCommand(CREATE_DIRECTORY);
-	message->setParameter2(storageID.raw);
+	message->setParameter2(storeId.raw);
 }
 
 void FileSystemMessage::setDeleteDirectoryCommand(
-		CommandMessage* message, store_address_t storageID) {
+		CommandMessage* message, store_address_t storeId) {
 	message->setCommand(DELETE_DIRECTORY);
-	message->setParameter2(storageID.raw);
+	message->setParameter2(storeId.raw);
 }
 
 void FileSystemMessage::setWriteCommand(CommandMessage* message,
-		store_address_t storageID) {
+		store_address_t storeId) {
 	message->setCommand(APPEND_TO_FILE);
-	message->setParameter2(storageID.raw);
+	message->setParameter2(storeId.raw);
 }
 
 void FileSystemMessage::setReadCommand(CommandMessage* message,
-		store_address_t storageID) {
+		store_address_t storeId) {
 	message->setCommand(READ_FROM_FILE);
-	message->setParameter2(storageID.raw);
+	message->setParameter2(storeId.raw);
 }
 
 void FileSystemMessage::setReadReply(CommandMessage* message,
 		store_address_t storageID) {
 	message->setCommand(READ_REPLY);
 	message->setParameter2(storageID.raw);
-}
-
-void FileSystemMessage::setCreateFileCommand(CommandMessage *message,
-        store_address_t storeId) {
-    message->setCommand(CREATE_FILE);
-    message->setParameter2(storeId.raw);
 }
 
 store_address_t FileSystemMessage::getStoreId(const CommandMessage* message) {
