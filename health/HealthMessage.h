01e7a98425f8fd09b08d8f0e8edbbe9bed695de6
Robin.Mueller <robin.mueller.m@gmail.com>
2020-10-10 17:55:38 +0200

    added announce all subservice

--- health/HealthMessage.h
+++ health/HealthMessage.h
@@ -7,17 +7,20 @@
 class HealthMessage {
 public:
 	static const uint8_t MESSAGE_ID = messagetypes::HEALTH_COMMAND;
-	// REPLY_COMMAND_OK/REPLY_REJECTED
+
 	static const Command_t HEALTH_SET = MAKE_COMMAND_ID(1);
-	// NO REPLY!
-	static const Command_t HEALTH_ANNOUNCE = MAKE_COMMAND_ID(3);
+	// No reply expected, health will be announced as event!
+	static const Command_t HEALTH_ANNOUNCE = MAKE_COMMAND_ID(2);
+	// Same as before, but all objects in health table will
+	// announce their health as events.
+	static const Command_t HEALTH_ANNOUNCE_ALL = MAKE_COMMAND_ID(3);
+
 	static const Command_t HEALTH_INFO = MAKE_COMMAND_ID(5);
 	static const Command_t REPLY_HEALTH_SET = MAKE_COMMAND_ID(6);
 
 	static void setHealthMessage(CommandMessage *message, Command_t command,
 			HasHealthIF::HealthState health,
 			HasHealthIF::HealthState oldHealth = HasHealthIF::FAULTY);
-
 	static void setHealthMessage(CommandMessage *message, Command_t command);
 
 	static HasHealthIF::HealthState getHealth(const CommandMessage *message);
