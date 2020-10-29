e35aebcd0af19c9f96fa6a173963737edd639fea
Robin.Mueller <robin.mueller.m@gmail.com>
2020-10-29 17:45:06 +0100

    HasREturnvalue update

--- returnvalues/HasReturnvaluesIF.h
+++ returnvalues/HasReturnvaluesIF.h
@@ -1,8 +1,8 @@
-#ifndef FRAMEWORK_RETURNVALUES_HASRETURNVALUESIF_H_
-#define FRAMEWORK_RETURNVALUES_HASRETURNVALUESIF_H_
+#ifndef FSFW_RETURNVALUES_HASRETURNVALUESIF_H_
+#define FSFW_RETURNVALUES_HASRETURNVALUESIF_H_
 
 #include "FwClassIds.h"
-#include <config/returnvalues/classIds.h>
+#include <returnvalues/classIds.h>
 #include <cstdint>
 
 #define MAKE_RETURN_CODE( number )	((INTERFACE_ID << 8) + (number))
@@ -15,9 +15,17 @@ public:
 	static const ReturnValue_t RETURN_FAILED = 1;
 	virtual ~HasReturnvaluesIF() {}
 
-	static ReturnValue_t makeReturnCode(uint8_t interfaceId, uint8_t number) {
+	/**
+	 * It is discouraged to use the input parameters 0,0 and 0,1 as this
+	 * will generate the RETURN_OK and RETURN_FAILED returnvalues.
+	 * @param interfaceId
+	 * @param number
+	 * @return
+	 */
+	static constexpr ReturnValue_t makeReturnCode(uint8_t interfaceId,
+			uint8_t number) {
 	    return (interfaceId << 8) + number;
 	}
 };
 
-#endif /* FRAMEWORK_RETURNVALUES_HASRETURNVALUESIF_H_ */
+#endif /* FSFW_RETURNVALUES_HASRETURNVALUESIF_H_ */
