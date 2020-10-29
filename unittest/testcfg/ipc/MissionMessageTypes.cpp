172fbd7e4840bcc2001fcf425684a552f2488ad1
Robin.Mueller <robin.mueller.m@gmail.com>
2020-10-29 15:13:03 +0100

    some changes taken over

--- unittest/testcfg/ipc/MissionMessageTypes.cpp
+++ unittest/testcfg/ipc/MissionMessageTypes.cpp
@@ -1,5 +1,6 @@
+#include "MissionMessageTypes.h"
+
 #include <fsfw/ipc/CommandMessage.h>
-#include <fsfw/unittest/config/ipc/MissionMessageTypes.h>
 
 void messagetypes::clearMissionMessage(CommandMessage* message) {
 	switch(message->getMessageType()) {
