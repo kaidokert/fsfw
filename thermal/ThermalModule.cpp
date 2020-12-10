5bad1142059af8a946fe57c0cad31284fd8496ea
Spacefish <robin.mueller.m@gmail.com>
2020-12-10 16:39:31 +0100

    thermal module set heating repaired

--- thermal/ThermalModule.cpp
+++ thermal/ThermalModule.cpp
@@ -256,15 +256,16 @@ bool ThermalModule::calculateModuleHeaterRequestAndSetModuleStatus(
 }
 
 void ThermalModule::setHeating(bool on) {
-//	GlobDataSet mySet;
-//	gp_int8_t writableTargetState(targetState.getDataPoolId(),
-//			&mySet, PoolVariableIF::VAR_WRITE);
-//	if (on) {
-//		writableTargetState = STATE_REQUEST_HEATING;
-//	} else {
-//		writableTargetState = STATE_REQUEST_PASSIVE;
-//	}
-//	mySet.commit(PoolVariableIF::VALID);
+	ReturnValue_t result = targetState.read();
+	if(result == HasReturnvaluesIF::RETURN_OK) {
+		if(on) {
+			targetState.value = STATE_REQUEST_HEATING;
+		}
+		else {
+			targetState.value = STATE_REQUEST_PASSIVE;
+		}
+	}
+	targetState.setValid(true);
 }
 
 void ThermalModule::updateTargetTemperatures(ThermalComponentIF* component,
