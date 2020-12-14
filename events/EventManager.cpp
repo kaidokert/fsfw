2100ae34f2c69d9a982e1d52d2f8068558daff9b
Robin Mueller <robin.mueller.m@gmail.com>
2020-12-14 20:49:17 +0100

    event folder taken over

--- events/EventManager.cpp
+++ events/EventManager.cpp
@@ -1,4 +1,6 @@
 #include "EventManager.h"
+#include "EventMessage.h"
+
 #include <FSFWConfig.h>
 #include "../serviceinterface/ServiceInterfaceStream.h"
 #include "../ipc/QueueFactory.h"
@@ -9,7 +11,6 @@
 // objects registering for certain events.
 // Each listener requires 1 or 2 EventIdMatcher and 1 or 2 ReportRangeMatcher.
 // So a good guess is 75 to a max of 100 pools required for each, which fits well.
-// This should be configurable..
 const LocalPool::LocalPoolConfig EventManager::poolConfig = {
         {fsfwconfig::FSFW_EVENTMGMR_MATCHTREE_NODES,
         		sizeof(EventMatchTree::Node)},
