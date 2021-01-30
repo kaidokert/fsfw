cf945d4d34fc7cd99c17138d788ef1dc153e5b30
Robin Mueller <robin.mueller.m@gmail.com>
2021-01-30 20:29:23 +0100

    added srv20 written by J. Gerhards

---
+++ pus/Service20ParameterManagement.h
@@ -0,0 +1,58 @@
+#ifndef FSFW_PUS_SERVICE20PARAMETERMANAGEMENT_H_
+#define FSFW_PUS_SERVICE20PARAMETERMANAGEMENT_H_
+
+#include <fsfw/tmtcservices/CommandingServiceBase.h>
+
+/**
+ * @brief   PUS Service 20 Parameter Service implementation
+ * @details
+ * @author  J. Gerhards
+ *
+ */
+class Service20ParameterManagement : public CommandingServiceBase
+{
+public:
+    Service20ParameterManagement(object_id_t objectId, uint16_t apid, uint8_t serviceId,
+            uint8_t numberOfParallelCommands = 4, uint16_t commandTimeoutSeconds = 60);
+    virtual ~Service20ParameterManagement();
+
+    static constexpr uint8_t NUM_OF_PARALLEL_COMMANDS = 4;
+    static constexpr uint16_t COMMAND_TIMEOUT_SECONDS = 60;
+protected:
+    /* CommandingServiceBase (CSB) abstract functions. See CSB documentation. */
+    ReturnValue_t isValidSubservice(uint8_t subservice) override;
+    ReturnValue_t getMessageQueueAndObject(uint8_t subservice,
+            const uint8_t *tcData, size_t tcDataLen, MessageQueueId_t *id,
+            object_id_t *objectId) override;
+    ReturnValue_t prepareCommand(CommandMessage* message, uint8_t subservice,
+            const uint8_t *tcData, size_t tcDataLen, uint32_t *state,
+            object_id_t objectId) override;
+    ReturnValue_t handleReply(const CommandMessage* reply,
+            Command_t previousCommand, uint32_t *state,
+            CommandMessage* optionalNextCommand, object_id_t objectId,
+            bool *isStep) override;
+
+private:
+
+    ReturnValue_t checkAndAcquireTargetID(object_id_t* objectIdToSet,
+            const uint8_t* tcData, size_t tcDataLen);
+    ReturnValue_t checkInterfaceAndAcquireMessageQueue(
+            MessageQueueId_t* messageQueueToSet, object_id_t* objectId);
+    ReturnValue_t prepareDirectCommand(CommandMessage* message,
+            const uint8_t* tcData, size_t tcDataLen);
+
+    ReturnValue_t prepareDumpCommand(CommandMessage* message,
+            const uint8_t* tcData, size_t tcDataLen);
+    ReturnValue_t prepareLoadCommand(CommandMessage* message,
+            const uint8_t* tcData, size_t tcDataLen);
+
+    enum class Subservice {
+        PARAMETER_LOAD = 128, //!< [EXPORT] : Load a Parameter
+        PARAMETER_DUMP = 129, //!< [EXPORT] : Dump a Parameter
+        PARAMETER_DUMP_REPLY = 130, //!< [EXPORT] : Dump a Parameter
+    };
+
+};
+
+
+#endif /* FSFW_PUS_SERVICE20PARAMETERMANAGEMENT_H_ */
---
+++ pus/servicepackets/Service20Packets.h
@@ -0,0 +1,91 @@
+#ifndef FSFW_PUS_SERVICEPACKETS_SERVICE20PACKETS_H_
+#define FSFW_PUS_SERVICEPACKETS_SERVICE20PACKETS_H_
+
+#include <fsfw/parameters/HasParametersIF.h>
+#include <fsfw/serialize/SerialBufferAdapter.h>
+#include <fsfw/serialize/SerializeElement.h>
+#include <fsfw/serialize/SerialLinkedListAdapter.h>
+
+/**
+ * @brief
+ * @details
+ * @author
+ */
+class ParameterCommand: public SerialLinkedListAdapter<SerializeIF> { //!< [EXPORT] : [SUBSERVICE] 128, 129, 130
+public:
+    ParameterCommand(uint8_t* storePointer, size_t parameterDataLen):
+        parameterBuffer(storePointer, parameterDataLen) {
+        setLinks();
+    }
+
+    ParameterCommand(object_id_t objectId, ParameterId_t parameterId,
+            const uint8_t* parameterBuffer, size_t parameterBufferSize):
+                objectId(objectId), parameterId(parameterId),
+                parameterBuffer(parameterBuffer, parameterBufferSize) {
+        setLinks();
+    }
+
+    ParameterId_t getParameterId() const {
+        return parameterId.entry;
+    }
+
+    const uint8_t* getParameterBuffer() {
+        return parameterBuffer.entry.getConstBuffer();
+    }
+
+    size_t getParameterBufferLen() const {
+        return parameterBuffer.getSerializedSize();
+    }
+
+    uint8_t getPtc() const {
+        return ccsdsType.entry >> 8 & 0xff;
+    }
+
+    uint8_t getPfc() const {
+        return ccsdsType.entry & 0xff;
+    }
+
+    uint8_t getRows() const {
+        return rows.entry;
+    }
+
+    uint8_t getColumns() const {
+        return columns.entry;
+    }
+
+private:
+    void setLinks() {
+        setStart(&objectId);
+        objectId.setNext(&parameterId);
+        parameterId.setNext(&ccsdsType);
+        ccsdsType.setNext(&rows);
+        rows.setNext(&columns);
+        columns.setNext(&parameterBuffer);
+    }
+
+    SerializeElement<object_id_t> objectId = 0;
+    SerializeElement<ParameterId_t> parameterId = 0;
+    //! [EXPORT] : [COMMENT] Type consisting of one byte PTC and one byte PFC.
+    SerializeElement<uint16_t> ccsdsType = 0;
+    SerializeElement<uint8_t> columns = 0;
+    SerializeElement<uint8_t> rows = 0;
+    SerializeElement<SerialBufferAdapter<uint8_t>> parameterBuffer;
+};
+
+class ParameterLoadCommand: public ParameterCommand {
+public:
+    ParameterLoadCommand(uint8_t* parameterPacket, size_t parameterDataLen):
+        ParameterCommand(parameterPacket, parameterDataLen) {
+    }
+};
+
+class ParameterDumpReply: public ParameterCommand {
+public:
+    ParameterDumpReply(object_id_t objectId, ParameterId_t parameterId,
+            const uint8_t* parameterBuffer, size_t parameterBufferSize):
+                ParameterCommand(objectId, parameterId, parameterBuffer,
+                        parameterBufferSize) {
+    }
+};
+
+#endif /* FSFW_PUS_SERVICEPACKETS_SERVICE20PACKETS_H_ */
