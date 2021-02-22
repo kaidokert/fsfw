144375827441b80a601579fe4395e3bbc19df92c
Robin Mueller <robin.mueller.m@gmail.com>
2021-02-22 18:43:09 +0100

    added new static function to print missed deadline

--- osal/linux/TaskFactory.cpp
+++ osal/linux/TaskFactory.cpp
@@ -39,5 +39,26 @@ ReturnValue_t TaskFactory::delayTask(uint32_t delayMs){
 	return PosixThread::sleep(delayMs*1000000ull);
 }
 
+void TaskFactory::printMissedDeadline() {
+    char name[20] = {0};
+    int status = pthread_getname_np(pthread_self(), name, sizeof(name));
+#if FSFW_CPP_OSTREAM_ENABLED == 1
+    if(status == 0) {
+        sif::warning << "task::printMissedDeadline: " << name << "" << std::endl;
+    }
+    else {
+        sif::warning << "task::printMissedDeadline: Unknown task name" << status <<
+                std::endl;
+    }
+#else
+    if(status == 0) {
+        sif::printWarning("task::printMissedDeadline: %s\n", name);
+    }
+    else {
+        sif::printWarning("task::printMissedDeadline: Unknown task name\n", name);
+    }
+#endif /* FSFW_CPP_OSTREAM_ENABLED == 1 */
+}
+
 TaskFactory::TaskFactory() {
 }
