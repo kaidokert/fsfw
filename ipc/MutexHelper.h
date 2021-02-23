a3d245f5a030c9450c516552283d26da2c799301
Robin Mueller <robin.mueller.m@gmail.com>
2021-02-23 11:28:12 +0100

    mutex helper print support and nullptr check

--- ipc/MutexHelper.h
+++ ipc/MutexHelper.h
@@ -2,33 +2,44 @@
 #define FRAMEWORK_IPC_MUTEXHELPER_H_
 
 #include "MutexFactory.h"
-#include "../serviceinterface/ServiceInterfaceStream.h"
+#include "../serviceinterface/ServiceInterface.h"
 
 class MutexHelper {
 public:
-	MutexHelper(MutexIF* mutex, MutexIF::TimeoutType timeoutType =
-			MutexIF::TimeoutType::BLOCKING, uint32_t timeoutMs = 0) :
-			internalMutex(mutex) {
-		ReturnValue_t status = mutex->lockMutex(timeoutType,
-		        timeoutMs);
-		if(status == MutexIF::MUTEX_TIMEOUT) {
+    MutexHelper(MutexIF* mutex, MutexIF::TimeoutType timeoutType =
+            MutexIF::TimeoutType::BLOCKING, uint32_t timeoutMs = 0):
+            internalMutex(mutex) {
+        if(mutex == nullptr) {
+            return;
+        }
+        ReturnValue_t status = mutex->lockMutex(timeoutType,
+                timeoutMs);
+        if(status == MutexIF::MUTEX_TIMEOUT) {
 #if FSFW_CPP_OSTREAM_ENABLED == 1
-			sif::error << "MutexHelper: Lock of mutex failed with timeout of "
-					<< timeoutMs << " milliseconds!" << std::endl;
+            sif::error << "MutexHelper: Lock of mutex failed with timeout of "
+                    << timeoutMs << " milliseconds!" << std::endl;
+#else
+            sif::printError("MutexHelper: Lock of mutex failed with timeout of %lu milliseconds\n",
+                    timeoutMs);
 #endif
-		}
-		else if(status != HasReturnvaluesIF::RETURN_OK){
+        }
+        else if(status != HasReturnvaluesIF::RETURN_OK){
 #if FSFW_CPP_OSTREAM_ENABLED == 1
-			sif::error << "MutexHelper: Lock of Mutex failed with code "
-			        << status << std::endl;
+            sif::error << "MutexHelper: Lock of Mutex failed with code "
+                    << status << std::endl;
+#else
+            sif::printError("MutexHelper: Lock of Mutex failed with code %d\n", status);
 #endif
-		}
-	}
+        }
+    }
 
-	~MutexHelper() {
-		internalMutex->unlockMutex();
-	}
+    ~MutexHelper() {
+        if(internalMutex != nullptr) {
+            internalMutex->unlockMutex();
+        }
+    }
 private:
-	MutexIF* internalMutex;
+    MutexIF* internalMutex;
 };
+
 #endif /* FRAMEWORK_IPC_MUTEXHELPER_H_ */
