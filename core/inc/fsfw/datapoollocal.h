5adb5cce95929854396240e03229c0c610314286
Robin Mueller <robin.mueller.m@gmail.com>
2021-07-13 16:35:57 +0200

    restructuring fsfw

---
+++ core/inc/fsfw/datapoollocal.h
@@ -0,0 +1,12 @@
+#ifndef FSFW_DATAPOOLLOCAL_DATAPOOLLOCAL_H_
+#define FSFW_DATAPOOLLOCAL_DATAPOOLLOCAL_H_
+
+/* Collected related headers */
+#include "datapoollocal/LocalPoolVariable.h"
+#include "datapoollocal/LocalPoolVector.h"
+#include "datapoollocal/StaticLocalDataSet.h"
+#include "datapoollocal/LocalDataSet.h"
+#include "datapoollocal/SharedLocalDataSet.h"
+
+
+#endif /* FSFW_DATAPOOLLOCAL_DATAPOOLLOCAL_H_ */
