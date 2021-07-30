0b207b2b1a73f098db898d2b4f4cbba3855a7c4b
Robin Mueller <robin.mueller.m@gmail.com>
2021-07-30 14:18:47 +0200

    updated user folder

--- src/fsfw/serviceinterface/ServiceInterface.h
+++ src/fsfw/serviceinterface/ServiceInterface.h
@@ -1,7 +1,7 @@
 #ifndef FSFW_SERVICEINTERFACE_SERVICEINTERFACE_H_
 #define FSFW_SERVICEINTERFACE_SERVICEINTERFACE_H_
 
-#include <FSFWConfig.h>
+#include "fsfw/FSFW.h"
 #include "serviceInterfaceDefintions.h"
 
 #if FSFW_CPP_OSTREAM_ENABLED == 1
--- tests/user/testcfg/pollingsequence/PollingSequenceFactory.cpp
+++ tests/user/testcfg/pollingsequence/PollingSequenceFactory.cpp
@@ -1,6 +1,8 @@
 #include "PollingSequenceFactory.h"
 
-#include <fsfw/serviceinterface/ServiceInterfaceStream.h>
+#include <TestsConfig.h>
+
+#include <fsfw/serviceinterface/ServiceInterface.h>
 #include <fsfw/devicehandlers/DeviceHandlerIF.h>
 #include <fsfw/tasks/FixedTimeslotTaskIF.h>
 
@@ -10,14 +12,26 @@ ReturnValue_t pst::pollingSequenceInitDefault(
 	uint32_t length = thisSequence->getPeriodMs();
 
 	/* Add polling sequence table here */
+	thisSequence->addSlot(objects::TEST_DEVICE, 0,
+			DeviceHandlerIF::PERFORM_OPERATION);
+	thisSequence->addSlot(objects::TEST_DEVICE, 0.3,
+			DeviceHandlerIF::SEND_WRITE);
+	thisSequence->addSlot(objects::TEST_DEVICE, 0.45 * length,
+			DeviceHandlerIF::GET_WRITE);
+	thisSequence->addSlot(objects::TEST_DEVICE, 0.6 * length,
+			DeviceHandlerIF::SEND_READ);
+	thisSequence->addSlot(objects::TEST_DEVICE, 0.8 * length,
+			DeviceHandlerIF::GET_READ);
 
 	if (thisSequence->checkSequence() == HasReturnvaluesIF::RETURN_OK) {
 		return HasReturnvaluesIF::RETURN_OK;
 	}
 	else {
-#if FSFW_CPP_OSTREAM_ENABLED == 1
+#if FSFW_CPP_OSTREAM_ENABLED
 		sif::error << "pst::pollingSequenceInitDefault: Sequence invalid!"
 		        << std::endl;
+#else
+		sif::printError("pst::pollingSequenceInitDefault: Sequence invalid!");
 #endif
 		return HasReturnvaluesIF::RETURN_FAILED;
 	}
