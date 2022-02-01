d2b561ba2f068a80b0239b072fbb8c8919ea9535
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-02-01 13:57:27 +0100

    test

--- tests/src/fsfw_tests/unit/hal/testCommandExecutor.cpp
+++ tests/src/fsfw_tests/unit/hal/testCommandExecutor.cpp
@@ -59,7 +59,8 @@ TEST_CASE( "Command Executor" , "[cmd-exec]") {
     REQUIRE(outputBuffer.getAvailableReadData() == 12);
     uint8_t readBuffer[32];
     REQUIRE(outputBuffer.readData(readBuffer, 12) == HasReturnvaluesIF::RETURN_OK);
-    CHECK(strcmp(reinterpret_cast<char*>(readBuffer), "Hello World\n") == 0);
+    std::string cmpString = "Hello World\n";
+    CHECK(strcmp(reinterpret_cast<char*>(readBuffer), cmpString.c_str()) == 0);
     outputBuffer.deleteData(12, true);
     // Test more complex command
     result = cmdExecutor.load("ping -c 1 localhost", false, false);
