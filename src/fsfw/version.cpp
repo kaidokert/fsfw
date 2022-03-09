345a799031560947445e404e79ce88c1f21fffb1
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-03-09 19:05:07 +0100

    improved version.h

--- src/fsfw/version.cpp
+++ src/fsfw/version.cpp
@@ -1,8 +1,11 @@
 #include "version.h"
+
 #include "fsfw/FSFWVersion.h"
 
-void fsfw::getVersion(Version& v) {
-  v.major = FSFW_VERSION;
-  v.minor = FSFW_SUBVERSION;
-  v.revision = FSFW_REVISION;
-}
+const fsfw::Version fsfw::FSFW_VERSION = {FSFW_VERSION_MAJOR, FSFW_VERSION_MINOR,
+                                          FSFW_VERSION_REVISION};
+
+fsfw::Version::Version(uint32_t major, uint32_t minor, uint32_t revision)
+    : major(major), minor(minor), revision(revision) {}
+
+void fsfw::getVersion(Version& version) {}
--- src/fsfw/version.h
+++ src/fsfw/version.h
@@ -5,14 +5,35 @@
 
 namespace fsfw {
 
-struct Version {
+class Version {
+ public:
+  Version(uint32_t major, uint32_t minor, uint32_t revision);
   uint32_t major = 0;
   uint32_t minor = 0;
   uint32_t revision = 0;
+
+  friend bool operator==(const Version& v1, const Version& v2) {
+    return (v1.major == v2.major and v1.minor == v2.minor and v1.revision == v2.revision);
+  }
+
+  friend bool operator!=(const Version& v1, const Version& v2) { return not(v1 == v2); }
+
+  friend bool operator<(const Version& v1, const Version& v2) {
+    return ((v1.major < v2.major) or (v1.major == v2.major and v1.minor < v2.minor) or
+            (v1.major == v2.major and v1.minor == v2.minor and v1.revision < v2.revision));
+  }
+
+  friend bool operator>(const Version& v1, const Version& v2) { return not(v1 < v2); }
+
+  friend bool operator<=(const Version& v1, const Version& v2) { return ((v1 == v2) or (v1 < v2)); }
+
+  friend bool operator>=(const Version& v1, const Version& v2) { return ((v1 == v2) or (v1 > v2)); }
 };
+
+extern const fsfw::Version FSFW_VERSION;
 
 void getVersion(Version& version);
 
-}
+}  // namespace fsfw
 
 #endif /* FSFW_SRC_FSFW_VERSION_H_ */
