0bdcb40609e62c9dd04daa85c48db09edf93beb9
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-03-11 14:25:01 +0100

    minor event changes

--- src/fsfw/events/Event.h
+++ src/fsfw/events/Event.h
@@ -7,8 +7,19 @@
 // could be moved to more suitable location
 #include <events/subsystemIdRanges.h>
 
-typedef uint16_t EventId_t;
-typedef uint8_t EventSeverity_t;
+using EventId_t = uint16_t;
+using EventSeverity_t = uint8_t ;
+using UniqueEventId_t = uint8_t;
+
+namespace severity {
+enum Severity: EventSeverity_t {
+  INFO = 1,
+  LOW = 2,
+  MEDIUM = 3,
+  HIGH = 4
+};
+
+}  // namespace severity
 
 #define MAKE_EVENT(id, severity) (((severity) << 16) + (SUBSYSTEM_ID * 100) + (id))
 
@@ -20,18 +31,11 @@ constexpr EventId_t getEventId(Event event) { return (event & 0xFFFF); }
 
 constexpr EventSeverity_t getSeverity(Event event) { return ((event >> 16) & 0xFF); }
 
-constexpr Event makeEvent(uint8_t subsystemId, uint8_t uniqueEventId,
+constexpr Event makeEvent(uint8_t subsystemId, UniqueEventId_t uniqueEventId,
                           EventSeverity_t eventSeverity) {
   return (eventSeverity << 16) + (subsystemId * 100) + uniqueEventId;
 }
 
 }  // namespace event
-
-namespace severity {
-static constexpr EventSeverity_t INFO = 1;
-static constexpr EventSeverity_t LOW = 2;
-static constexpr EventSeverity_t MEDIUM = 3;
-static constexpr EventSeverity_t HIGH = 4;
-}  // namespace severity
 
 #endif /* EVENTOBJECT_EVENT_H_ */
