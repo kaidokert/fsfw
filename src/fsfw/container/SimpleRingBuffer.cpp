a891769a02949d82a144bc5ea8f0e6522b0ce14e
Robin Mueller <robin.mueller.m@gmail.com>
2022-03-24 11:32:27 +0100

    ring buffer cfg error warning

--- src/fsfw/container/SimpleRingBuffer.cpp
+++ src/fsfw/container/SimpleRingBuffer.cpp
@@ -1,4 +1,7 @@
 #include "fsfw/container/SimpleRingBuffer.h"
+#include "fsfw/FSFW.h"
+
+#include "fsfw/serviceinterface.h"
 
 #include <cstring>
 
@@ -48,7 +51,14 @@ void SimpleRingBuffer::confirmBytesWritten(size_t amount) {
 }
 
 ReturnValue_t SimpleRingBuffer::writeData(const uint8_t* data, size_t amount) {
-  if(amount > maxSize()) {
+  if(amount > getMaxSize()) {
+#if FSFW_VERBOSE_LEVEL >= 1
+#if FSFW_CPP_OSTREAM_ENABLED == 1
+      sif::error << "SimpleRingBuffer::writeData: Amount of data too large" << std::endl;
+#else
+      sif::printError("SimpleRingBuffer::writeData: Amount of data too large\n");
+#endif
+#endif
     return HasReturnvaluesIF::RETURN_FAILED;
   }
   if (availableWriteSpace() >= amount or overwriteOld) {
