82fc7f33a8b27b985f279b8213bc9df7340865f2
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-03-29 15:11:41 +0200

    update afmt scripts to format *.tpp files

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -3,6 +3,6 @@ if [[ ! -f README.md ]]; then
 	cd ..
 fi
 
-find ./src -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
-find ./hal -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
-find ./tests -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
+find ./src -iname *.h -o -iname *.cpp -o -iname *.c -o -iname *.tpp | xargs clang-format --style=file -i
+find ./hal -iname *.h -o -iname *.cpp -o -iname *.c -o -iname *.tpp | xargs clang-format --style=file -i
+find ./tests -iname *.h -o -iname *.cpp -o -iname *.c -o -iname *.tpp | xargs clang-format --style=file -i
