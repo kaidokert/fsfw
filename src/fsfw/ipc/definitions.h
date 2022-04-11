8c2105ae0a98d9b241f7ee282c0be72b9e59ad4b
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-04-11 14:00:37 +0200

    correct init value for object ID

--- src/fsfw/ipc/definitions.h
+++ src/fsfw/ipc/definitions.h
@@ -1,11 +1,13 @@
 #ifndef FSFW_SRC_FSFW_IPC_DEFINITIONS_H_
 #define FSFW_SRC_FSFW_IPC_DEFINITIONS_H_
+
 #include <fsfw/objectmanager/SystemObjectIF.h>
+#include <fsfw/objectmanager/frameworkObjects.h>
 
 struct MqArgs {
   MqArgs(){};
   MqArgs(object_id_t objectId, void* args = nullptr) : objectId(objectId), args(args) {}
-  object_id_t objectId = 0;
+  object_id_t objectId = objects::NO_OBJECT;
   void* args = nullptr;
 };
 
