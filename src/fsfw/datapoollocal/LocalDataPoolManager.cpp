acc4c8d975bb05c704ad94613ea10782f707d106
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-04-11 14:33:28 +0200

    check serialize result as well

--- src/fsfw/datapoollocal/LocalDataPoolManager.cpp
+++ src/fsfw/datapoollocal/LocalDataPoolManager.cpp
@@ -787,6 +787,10 @@ ReturnValue_t LocalDataPoolManager::generateSetStructurePacket(sid_t sid, bool i
   // Serialize set packet into store.
   size_t size = 0;
   result = setPacket.serialize(&storePtr, &size, expectedSize, SerializeIF::Endianness::BIG);
+  if(result != HasReturnvaluesIF::RETURN_OK) {
+      ipcStore->deleteData(storeId);
+      return result;
+  }
   if (expectedSize != size) {
     printWarningOrError(sif::OutputTypes::OUT_WARNING, "generateSetStructurePacket",
                         HasReturnvaluesIF::RETURN_FAILED,
