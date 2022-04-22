a569990ca2683c537acb88b4866962e57f9a054c
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-04-22 14:50:50 +0200

    fix tests

--- tests/src/fsfw_tests/unit/version.cpp
+++ tests/src/fsfw_tests/unit/version.cpp
@@ -10,12 +10,12 @@ TEST_CASE("Version API Tests", "[TestVersionAPI]") {
   // Check that major version is non-zero
   REQUIRE(fsfw::FSFW_VERSION.major > 0);
   uint32_t fsfwMajor = fsfw::FSFW_VERSION.major;
-  REQUIRE(fsfw::Version(255, 0, 0) > fsfw::FSFW_VERSION);
-  REQUIRE(fsfw::Version(255, 0, 0) >= fsfw::FSFW_VERSION);
-  REQUIRE(fsfw::Version(0, 0, 0) < fsfw::FSFW_VERSION);
-  REQUIRE(fsfw::Version(0, 0, 0) <= fsfw::FSFW_VERSION);
-  fsfw::Version v1 = fsfw::Version(1, 1, 1);
-  fsfw::Version v2 = fsfw::Version(1, 1, 1);
+  REQUIRE(Version(255, 0, 0) > fsfw::FSFW_VERSION);
+  REQUIRE(Version(255, 0, 0) >= fsfw::FSFW_VERSION);
+  REQUIRE(Version(0, 0, 0) < fsfw::FSFW_VERSION);
+  REQUIRE(Version(0, 0, 0) <= fsfw::FSFW_VERSION);
+  Version v1 = fsfw::Version(1, 1, 1);
+  Version v2 = fsfw::Version(1, 1, 1);
   REQUIRE(v1 == v2);
   REQUIRE(not(v1 < v2));
   REQUIRE(not(v1 > v2));
