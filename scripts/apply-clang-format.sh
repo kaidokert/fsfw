01ebf0f4d3dd9969742a27748540d312f81d9088
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-05-13 11:49:01 +0200

    tab size

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 if [[ ! -f README.md ]]; then
-	cd ..
+  cd ..
 fi
 
 cmake_fmt="cmake-format"
 if command -v ${cmake_fmt} &> /dev/null; then
-	cmake_fmt_cmd="${cmake_fmt} -i CMakeLists.txt"
-	eval ${cmake_fmt_cmd}
+  cmake_fmt_cmd="${cmake_fmt} -i CMakeLists.txt"
+  eval ${cmake_fmt_cmd}
 else
-	echo "No ${cmake_fmt} tool found, not formatting CMake files"
+  echo "No ${cmake_fmt} tool found, not formatting CMake files"
 fi
 
 cpp_format="clang-format"
 if command -v ${cpp_format} &> /dev/null; then
-	find ./src -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
-	find ./hal -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
-	find ./tests -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
+  find ./src -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
+  find ./hal -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
+  find ./tests -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
 else
-	echo "No ${cpp_format} tool found, not formatting C++/C files"
+  echo "No ${cpp_format} tool found, not formatting C++/C files"
 fi
