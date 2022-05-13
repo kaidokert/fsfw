994c7299b979e62cdcc7eff985423a093e1603d2
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-05-13 11:34:44 +0200

    add cmake-format command to shell script

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -3,6 +3,12 @@ if [[ ! -f README.md ]]; then
 	cd ..
 fi
 
+cmake_fmt="python3 -m cmake-format"
+if command -v ${cmake_fmt} &> /dev/null
+	cmake_fmt_cmd=${cmake_fmt} -i CMakeLists.txt
+	eval ${cmake_fmt_cmd}
+then
+
 find ./src -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
 find ./hal -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
 find ./tests -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
