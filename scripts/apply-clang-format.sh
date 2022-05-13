99fe6487c8d5a5fe3934afbcda2b8268365ea6aa
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-05-13 11:48:18 +0200

    another small improvement

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -8,7 +8,7 @@ if command -v ${cmake_fmt} &> /dev/null; then
 	cmake_fmt_cmd="${cmake_fmt} -i CMakeLists.txt"
 	eval ${cmake_fmt_cmd}
 else
-	echo "No cmake-format tool found, not formatting CMake files"
+	echo "No ${cmake_fmt} tool found, not formatting CMake files"
 fi
 
 cpp_format="clang-format"
@@ -17,5 +17,5 @@ if command -v ${cpp_format} &> /dev/null; then
 	find ./hal -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
 	find ./tests -iname *.h -o -iname *.cpp -o -iname *.c | xargs clang-format --style=file -i
 else
-	echo "No clang-format tool found, not formatting C++/C files"
+	echo "No ${cpp_format} tool found, not formatting C++/C files"
 fi
