1a294e6a130b8b8e613d4ecf1547a249b1b39184
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-06-20 15:40:15 +0200

    include TestsConfig.h

--- tests/src/fsfw_tests/unit/hal/testCommandExecutor.cpp
+++ tests/src/fsfw_tests/unit/hal/testCommandExecutor.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 
+#include "TestsConfig.h"
 #include "fsfw/container/DynamicFIFO.h"
 #include "fsfw/container/SimpleRingBuffer.h"
 #include "fsfw/platform.h"
