0a38d2e22d75fdfc6b2ea40449eb706055246b03
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-07-27 17:56:16 +0200

    small bugfix

--- src/fsfw/tmtcpacket/pus/tm/PusTmZcWriter.cpp
+++ src/fsfw/tmtcpacket/pus/tm/PusTmZcWriter.cpp
@@ -10,8 +10,9 @@ void PusTmZeroCopyWriter::setSequenceCount(uint16_t seqCount) {
       reinterpret_cast<ccsds::PrimaryHeader*>(const_cast<uint8_t*>((pointers.spHeaderStart)));
   ccsds::setSequenceCount(*spHeader, seqCount);
 }
+
 void PusTmZeroCopyWriter::updateErrorControl() {
-  auto* crcStart = reinterpret_cast<uint8_t*>(const_cast<uint8_t*>((pointers.spHeaderStart)));
+  auto* crcStart = const_cast<uint8_t*>((pointers.crcStart));
   uint16_t crc16 =
       CRC::crc16ccitt(PusTmReader::getFullData(), getFullPacketLen() - sizeof(ecss::PusChecksumT));
   crcStart[0] = (crc16 >> 8) & 0xff;
