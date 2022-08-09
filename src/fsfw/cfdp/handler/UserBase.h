7a204123053c4a80697d1c33d9ee6904bb1df1af
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-08-09 15:51:01 +0200

    add user base documentation

--- src/fsfw/cfdp/handler/UserBase.h
+++ src/fsfw/cfdp/handler/UserBase.h
@@ -7,6 +7,12 @@ namespace cfdp {
 
 class UserBase {
  public:
+  /**
+   * Create a user base class which is used to provides a user interface to interact with CFDP
+   * handlers. It is also used to pass the Virtual Filestore (VFS) Implementation to the CFDP
+   * handlers so the filestore operations can be mapped to the underlying filestore.
+   * @param vfs Virtual Filestore Object. Will be used for all file operations
+   */
   explicit UserBase(HasFileSystemIF& vfs);
 
  private:
