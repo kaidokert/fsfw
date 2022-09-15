c549914efb845756800cc32d33b03d87e386b11e
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-09-15 16:50:06 +0200

    include helpers

--- src/fsfw/cfdp.h
+++ src/fsfw/cfdp.h
@@ -4,6 +4,7 @@
 #include "cfdp/definitions.h"
 #include "cfdp/handler/DestHandler.h"
 #include "cfdp/handler/FaultHandlerBase.h"
+#include "cfdp/helpers.h"
 #include "cfdp/tlv/Lv.h"
 #include "cfdp/tlv/StringLv.h"
 #include "cfdp/tlv/Tlv.h"
